ea67f309a73c998ce786f3f063e78784428d9f43
Thomas A. Werne <werneta@gmail.com>
2024-04-07 23:37:37 -0700

    Rough-out template serialization logic

--- src/midi.rs
+++ src/midi.rs
@@ -1,3 +1,5 @@
+pub const NOVATION_SYSEX_ID: [u8; 3] = [0x00, 0x20, 0x29];
+
 pub fn sysex(id: [u8; 3], msg: Vec<u8>) -> Vec<u8> {
     let mut rv: Vec<u8> = vec![];
 
--- src/template.rs
+++ src/template.rs
@@ -1,5 +1,5 @@
 use crate::controls::{defaults, Button, Fader, Footswitch, Key, Pad, Pedal, Rotary, Wheel};
-use crate::midi::sysex;
+use crate::midi::{sysex, NOVATION_SYSEX_ID};
 use std::iter::repeat;
 
 /******************************************************************************/
@@ -46,13 +46,44 @@ pub fn default() -> Template {
 
 pub fn serialize(template: &Template) -> Vec<u8> {
     let mut rv: Vec<u8> = vec![];
-    let novation_sysex_id = [0x00, 0x20, 0x29];
     let ser_len = 4214; // Bytes
 
+    // Bogus filler logic, capturing the 16 messages
     let mut msg: Vec<u8> = vec![];
     msg.extend(repeat(0).take(4214 - 2 - 3));
-    let msg1 = sysex(novation_sysex_id, msg);
+    let msg0 = sysex(NOVATION_SYSEX_ID, msg);
+    let msg1 = sysex(NOVATION_SYSEX_ID, msg);
+    let msg2 = sysex(NOVATION_SYSEX_ID, msg);
+    let msg3 = sysex(NOVATION_SYSEX_ID, msg);
+    let msg4 = sysex(NOVATION_SYSEX_ID, msg);
+    let msg5 = sysex(NOVATION_SYSEX_ID, msg);
+    let msg6 = sysex(NOVATION_SYSEX_ID, msg);
+    let msg7 = sysex(NOVATION_SYSEX_ID, msg);
+    let msg8 = sysex(NOVATION_SYSEX_ID, msg);
+    let msg9 = sysex(NOVATION_SYSEX_ID, msg);
+    let msg10 = sysex(NOVATION_SYSEX_ID, msg);
+    let msg11 = sysex(NOVATION_SYSEX_ID, msg);
+    let msg12 = sysex(NOVATION_SYSEX_ID, msg);
+    let msg13 = sysex(NOVATION_SYSEX_ID, msg);
+    let msg14 = sysex(NOVATION_SYSEX_ID, msg);
+    let msg15 = sysex(NOVATION_SYSEX_ID, msg);
 
+    rv.extend(msg0);
     rv.extend(msg1);
+    rv.extend(msg2);
+    rv.extend(msg3);
+    rv.extend(msg4);
+    rv.extend(msg5);
+    rv.extend(msg6);
+    rv.extend(msg7);
+    rv.extend(msg8);
+    rv.extend(msg9);
+    rv.extend(msg10);
+    rv.extend(msg11);
+    rv.extend(msg12);
+    rv.extend(msg13);
+    rv.extend(msg14);
+    rv.extend(msg15);
+
     return rv;
 }
