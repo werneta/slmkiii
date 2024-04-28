8e760064d40f45994928b617650ffd4dddada390
Thomas A. Werne <werneta@gmail.com>
2024-04-27 17:05:21 -0700

    Remove stubbed rotary, button, pad logic

--- src/controls/rotary.rs
+++ src/controls/rotary.rs
@@ -14,26 +14,3 @@ pub struct RotaryBehavior {
 }
 
 /******************************************************************************/
-
-#[derive(Debug)]
-pub struct Rotary {
-    name: String,
-    enabled: bool,
-    behavior: RotaryBehavior,
-    assignment: Assignment1,
-    pivot: u8, // 0-127
-}
-
-/******************************************************************************/
-
-impl Rotary {
-    pub fn serialize(self: &Rotary) -> Vec<u8> {
-        let mut rv: Vec<u8> = vec![0];
-
-        rv.push(if self.enabled { 1 } else { 0 });
-        rv.extend(self.name.as_bytes());
-        rv.extend(zeros(9 - self.name.len()));
-
-        return rv;
-    }
-}
